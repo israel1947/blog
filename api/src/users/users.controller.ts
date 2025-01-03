7be0f7a57855cb63a48d32b052e84466f253c1a2
israel1947 <israeldavidsdvsf@gmail.com>
2025-01-03 17:26:47 +0100

    feat(backend): backend
    - Se implementan pipes y decoradores de carga de archivo a la hora de actualizar la informacion de los usuarios

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,13 +1,17 @@
-import { Body, Controller, Delete, Get, Param, Patch, Query, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, FileTypeValidator, Get, Param, ParseFilePipe, Patch, Query, Res, UploadedFile, UseGuards, UseInterceptors, UsePipes, ValidationPipe } from '@nestjs/common';
 import { Response } from 'express';
 import { UsersService } from './users.service';
 import { UserDto } from 'src/dto/userDto';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { MailService } from 'src/mail/mail.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as multer from 'multer';
+import * as path from 'path';
+import * as uniqid from 'uniqid';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly userService: UsersService, private email:MailService) { }
+  constructor(private readonly userService: UsersService, private email: MailService) { }
 
 
   @Get()
@@ -32,8 +36,8 @@ export class UsersController {
       const isSubscribed = suscription === 'true';
       console.log(isSubscribed);
 
-     /*  const user = await this.userService.userSubscribed(isSubscribed); */
-     const user = await this.email.testEamil(isSubscribed);
+      /*  const user = await this.userService.userSubscribed(isSubscribed); */
+      const user = await this.email.testEamil(isSubscribed);
       resp.send({ ok: true, user: user });
     } catch (error) {
       resp.status(404).send({ ok: false, message: error.message });
@@ -42,9 +46,41 @@ export class UsersController {
 
 
   @UseGuards(AuthGuard)
+  @UsePipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transformOptions: {
+      enableImplicitConversion: true,
+    },
+  }))
+  @UseInterceptors(FileInterceptor('photo', {
+    storage: multer.diskStorage({
+      destination: (req, file, cb) => {
+        const uploadPath = path.resolve(__dirname, '../../uploads/user/profile');
+        cb(null, uploadPath);
+      },
+      filename: (req, file, cb) => {
+        const nameArr = file.originalname.split('.')
+        const extention = nameArr[nameArr.length - 1];
+        const uniqId = uniqid();
+        const uniqueName = `${uniqId}.${extention}`
+        cb(null, uniqueName);
+      },
+    }),
+  }))
   @Patch(':id')
-  async updateUser(@Param('id') id: string, @Res() resp: Response, @Body() userData: UserDto) {
+  async updateUser(
+    @Param('id') id: string,
+    @UploadedFile(new ParseFilePipe({
+      validators: [
+        new FileTypeValidator({ fileType: 'image/jpeg' })
+      ]
+    })) file: Express.Multer.File,
+    @Res() resp: Response,
+    @Body() userData: UserDto,
+  ) {
     try {
+      userData.photo = file.filename;
       const updatedUser = await this.userService.updateUser(id, userData);
       resp.send({ ok: true, messge: "User Updated Susscefully!", user: updatedUser });
     } catch (error) {
