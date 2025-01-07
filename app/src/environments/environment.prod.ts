03cb625345d8f3202dbf54d645381c667720bce6
israel1947 <israeldavidsdvsf@gmail.com>
2025-01-07 17:17:06 +0100

    feat(frontend): frontend
    - Se modifica en gitignore

---
+++ app/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  client_id:"1004271504512-euq8ju2v1t5tes9ofdrfk6ha7t05mkmc.apps.googleusercontent.com"
+};
---
+++ app/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  client_id:"1004271504512-euq8ju2v1t5tes9ofdrfk6ha7t05mkmc.apps.googleusercontent.com",
+  URL:'http://localhost:3000'
+};
