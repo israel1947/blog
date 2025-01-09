452cda5bea95e9ffac40a556a12cfdc378b62041
israel1947 <israeldavidsdvsf@gmail.com>
2025-01-09 14:06:29 +0100

    perf(frontend): frontend
    - Se modifica el enviroment.prod por el url del api en prod

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   client_id:"1004271504512-euq8ju2v1t5tes9ofdrfk6ha7t05mkmc.apps.googleusercontent.com",
-  URL:'http://localhost:3000'
+  URL:'https://blog-1-xgxo.onrender.com'
 };
